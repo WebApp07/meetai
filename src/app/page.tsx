f401ff065e7bc2d3ecb174ebd39bc63774422aa9
WebApp07 <aminezouguig@proton.me>
2026-02-08 23:22:47 +0100

    Integrate Better-Auth, Configure autj schema, Create basic UI, Create new user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,53 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
+
 export default function Home() {
-  return <div className="text-4xl font-bold text-green-500">Hello World!</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        name,
+        email,
+        password,
+      },
+      {
+        onError: () => {
+          alert("Error creating user");
+        },
+        onSuccess: () => {
+          alert("User created successfully");
+        },
+      },
+    );
+  };
+
+  return (
+    <div className="p-4 flex flex-col gap-y-4">
+      <Input
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <Button onClick={onSubmit}>Create a User</Button>
+    </div>
+  );
 }
---
+++ src/lib/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db";
+import * as schema from "@/db/schema";
+
+// Initialize better-auth with the Drizzle adapter
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: {
+      ...schema,
+    },
+  }),
+});
